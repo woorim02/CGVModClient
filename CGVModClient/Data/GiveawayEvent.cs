311229f35fb7d52a89b880b156cf631e0cc820ea
WooRim Jeon <99327851+woorim02@users.noreply.github.com>
2024-01-09 14:42:35 +0900

    Add Data Class

---
+++ CGVModClient/Data/GiveawayEvent.cs
@@ -0,0 +1,35 @@
+﻿namespace CGVModClient.Data;
+
+public class GiveawayEvent
+{
+    public string Title { get; init; }
+    public string EventIndex { get; init; }
+    public string Period { get; init; }
+    public string DDay { get; init; }
+
+    private DateTime? startDate;
+    public DateTime StartDate
+    {
+        get {
+            if (startDate == null)
+            {
+                try { startDate = DateTime.Parse(Period.Split('~')[0]); }
+                catch { startDate = DateTime.MinValue; }
+            }
+            return startDate.Value;
+        }
+    }
+
+    private DateTime? endDate;
+    public DateTime EndDate
+    {
+        get {
+            if (endDate == null)
+            {
+                try { endDate = DateTime.Parse(Period.Split('~')[1]); }
+                catch { endDate = DateTime.MinValue; }
+            }
+            return endDate.Value;
+        }
+    }
+}
---
+++ CGVModClient/Data/GiveawayEventModel.cs
@@ -0,0 +1,20 @@
+﻿namespace CGVModClient.Data;
+
+public class GiveawayEventModel
+{
+    public string Title { get => GiveawayItemList.GiveawayItemName; }
+    public string EventIndex { get; init; }
+    public string GivewayIndex { get => GiveawayItemList.GiveawayItemCode; }
+    public string Contents {  get; init; }
+
+    /// <summary>
+    /// Please do not use it. This is a property for json serialization.
+    /// </summary>
+    public GiveawayInfo GiveawayItemList { get; set; }
+
+    public class GiveawayInfo
+    {
+        public string GiveawayItemCode { get; set; }
+        public string GiveawayItemName { get; set; }
+    }
+}
