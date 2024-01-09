19baf340588c1ffff19055cfd4e2e3c769daf4f6
WooRim Jeon <99327851+woorim02@users.noreply.github.com>
2024-01-09 19:37:03 +0900

    경품이벤트 목록 뷰, 뷰모델 생성

--- CGVModClient/Pages/GiveawayEventListPage.xaml.cs
+++ CGVModClient/Pages/GiveawayEventListPage.xaml.cs
@@ -1,9 +1,24 @@
+using CGVModClient.ViewModels;
+
 namespace CGVModClient.Pages;
 
 public partial class GiveawayEventListPage : ContentPage
 {
-	public GiveawayEventListPage()
+    GiveawayEventListViewModel viewModel;
+    public GiveawayEventListPage()
 	{
 		InitializeComponent();
+		viewModel = new GiveawayEventListViewModel();
+        BindingContext = viewModel;
 	}
+
+    private async void ContentPage_Loaded(object sender, EventArgs e)
+    {
+        await viewModel.LoadAsync();
+    }
+
+    private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        
+    }
 }
--- CGVModClient/ViewModels/GiveawayEventListViewModel.cs
+++ CGVModClient/ViewModels/GiveawayEventListViewModel.cs
@@ -1,7 +1,33 @@
-﻿
+﻿using CGVModClient.Data;
+using CGVModClient.Model;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace CGVModClient.ViewModels;
 
-public class GiveawayEventListViewModel : ObservableObject
+public class GiveawayEventListViewModel : INotifyPropertyChanged
 {
+    CGVEventService eventService = new CGVEventService();
 
+    public string EventState { get; set; }
+    public List<GiveawayEvent> GiveawayEventList { get; set;}
+
+    public async Task LoadAsync()
+    {
+        var arr = await eventService.GetGiveawayEventsAsync();
+        GiveawayEventList = new List<GiveawayEvent>(arr);
+        SetEventState(GiveawayEventList.Count);
+        OnPropertyChanged(nameof(GiveawayEventList));
+        OnPropertyChanged(nameof(EventState));
+    }
+
+    public void SetEventState(int count)
+    {
+        EventState = $"진행중인 경품 이벤트 {count}개";
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
