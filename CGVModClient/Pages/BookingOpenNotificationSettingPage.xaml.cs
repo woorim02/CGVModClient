cb048306e4ae482ef805ad7a2745dd362a345804
WooRim Jeon <99327851+woorim02@users.noreply.github.com>
2024-01-14 23:16:06 +0900

    BookingOpenNotificationSettingPage, ViewModel 추가

---
+++ CGVModClient/Pages/BookingOpenNotificationSettingPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CGVModClient.Pages;
+
+public partial class BookingOpenNotificationSettingPage : ContentPage
+{
+	public BookingOpenNotificationSettingPage()
+	{
+		InitializeComponent();
+	}
+}
