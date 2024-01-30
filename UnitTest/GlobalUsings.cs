d3d7ae3ebaaf7d0e97a68ebf58a788e916032f2b
WooRim Jeon <99327851+woorim02@users.noreply.github.com>
2024-01-30 14:20:23 +0900

    유닛테스트 추가
    유닛테스트 추가
    
    유닛테스트 추가 2

---
+++ UnitTest/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Xunit;
+global using CGVModClient.Data;
+global using CGVModClient.Data.Json;
+global using CGVModClient.Model;
+global using CGVModClient.Pages;
+global using CGVModClient.Popup;
+global using CGVModClient.ViewModels;
+global using CommunityToolkit.Maui;
