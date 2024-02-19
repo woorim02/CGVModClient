d5f377b2aea5e2768e9f258dd0c7f04f224d47a7
WooRim Jeon <99327851+woorim02@users.noreply.github.com>
2024-02-20 00:44:21 +0900

    지역별 수량확인 기능 추가

--- CGVModClient/ViewModels/GiveawayEventDetailViewModel.cs
+++ CGVModClient/ViewModels/GiveawayEventDetailViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 
 namespace CGVModClient.ViewModels;
@@ -13,19 +14,35 @@ public partial class GiveawayEventDetailViewModel : ObservableObject
     private string title;
     [ObservableProperty]
     private string description;
-
+    [ObservableProperty]
+    public List<Area> areas;
     public ObservableCollection<Theater> Theaters { get; private set; } = new ObservableCollection<Theater>();
+
+    private GiveawayEventModel model;
     
     public async Task LoadAsync(string eventIndex)
     {
         IsBusy = true;
-        var eventModel = await service.Event.GetGiveawayEventModelAsync(eventIndex);
-        Title = eventModel.Title;
-        Description = eventModel.Contents;
-        var theaterInfo = await service.Event.GetGiveawayTheaterInfoAsync(eventModel.GiveawayIndex);
-        foreach(var item in theaterInfo.TheaterList) {
+        model = await service.Event.GetGiveawayEventModelAsync(eventIndex);
+        Title = model.Title;
+        Description = model.Contents;
+        var theaterInfo = await service.Event.GetGiveawayTheaterInfoAsync(model.GiveawayIndex);
+        Areas = theaterInfo.AreaList;
+        foreach (var item in theaterInfo.TheaterList) {
             Theaters.Add(item);
         }
         IsBusy = false;
+    }
+
+    [RelayCommand]
+    private async Task SelectArea(string areaCode)
+    {
+        IsBusy = true;
+        Theaters.Clear();
+        var theaterInfo = await service.Event.GetGiveawayTheaterInfoAsync(model.GiveawayIndex, areaCode);
+        foreach (var item in theaterInfo.TheaterList){
+            Theaters.Add(item);
+        }
+        IsBusy= false;
     }
 }
